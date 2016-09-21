14470f9db9472e55112e0dde5bbec381693417ac
Alireza Ghadimi <AlirezaGhadimy@Gmail.com>
2016-09-21 18:42:36 +0330

    Fix is_prime function

--- server/core/algorithms/algorithms.php
+++ server/core/algorithms/algorithms.php
@@ -86,12 +86,9 @@ class algorithms {
 	 * @param int $to
 	 *
 	 * @return int
-	 * @throws algorithmsException
+
 	 */
 	public static function factorial($n , $to = 1){
-		if($n > 500){
-			throw new algorithmsException('LargeNumber','factorial');
-		}
 		$result = 1;
 		for($i = $to;$i <= $n;$i++){
 			$result *= $i;
@@ -104,12 +101,8 @@ class algorithms {
 	 * @param $n
 	 *
 	 * @return float
-	 * @throws algorithmsException
 	 */
 	public static function fibonacci($n){
-		if($n > 1000){
-			throw new algorithmsException('LargeNumber','fibonacci');
-		}
 		return (static::fastexp(static::gr,$n) - (1/static::fastexp(-static::gr,$n))) / (2*static::gr-1);
 	}
 
@@ -117,22 +110,13 @@ class algorithms {
 	 * @param $a
 	 *
 	 * @return bool
-	 * @throws algorithmsException
 	 */
 	public static function  is_prime($a){
-		/**
-		 * Largest 8 bit number
-		 *  Dec: ‭4,294,967,295
-		 * It's not very large because this function takes lots of time‬
-		 */
-		if($a > 0XFFFFFFFF){
-			throw new algorithmsException('LargeNumber','is_prime');
-		}
 		if ($a == 1) return false;
 		if ($a == 2) return true;
 		if ($a % 2 == 0) return false;
 		$lim = (int)sqrt($a);
-		for ($i = 2; $i <= $lim; $i++) {
+		for ($i = 3; $i <= $lim; $i++) {
 			if ($a % $i == 0) {
 				return false;
 			}
