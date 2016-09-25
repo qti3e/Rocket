d5ea1c12be8e07f2141e6e67eeae073595e363a6
Alireza Ghadimi <AlirezaGhadimy@Gmail.com>
2016-09-26 00:23:02 +0330

    Write the Redis driver for logger library

---
+++ server/core/logger/drivers/redis.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Redis driver for logger library
+ *
+ * @license GPL
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 3
+ * @author  QTIƎE <Qti3eQti3e@Gmail.com>
+ */
+
+namespace core\logger\drivers;
+
+use core\config\config;
+use core\logger\driverInterface;
+use core\redis\redis as cRedis;
+
+/**
+ * Class redis
+ * @package core\logger\drivers
+ */
+class redis implements driverInterface {
+	/**
+	 * When it's true it means the instance of the cRedis was generated before
+	 * @var bool
+	 */
+	protected static $isConnected  = false;
+	/**
+	 * Instance of the Redis library
+	 * @var cRedis
+	 */
+	protected static $instance;
+
+	/**
+	 * Make a connection to the logger's Redis server
+	 * @return void
+	 */
+	protected function connect(){
+		$host       = config::get('logger_redis_host');
+		$port       = config::get('logger_redis_port');
+		$timeout    = config::get('logger_redis_timeout');
+		$db         = config::get('logger_redis_timeout');
+		$persistent = config::get('logger_redis_persistent');
+		$password   = config::get('logger_redis_password');
+		static::$instance   = new cRedis($host,$port,$timeout,$persistent,$db,$password);
+		static::$isConnected= true;
+	}
+
+	/**
+	 * Write a info to the log database
+	 * @param $message
+	 * @param $line
+	 * @param $file
+	 * @param $time
+	 *
+	 * @return string
+	 */
+	public function infoWriter($message, $line, $file, $time) {
+		if(!static::$isConnected)
+			$this->connect();
+		$id = hash(config::get('logger_redis_hash_algo'),uniqid('log_i_'));
+		static::$instance->hMSet($id,[
+			'message'   => $message,
+			'line'      => $line,
+			'file'      => $file,
+			'time'      => $time
+		]);
+		static::$instance->lPush('logs',$id);
+		static::$instance->lPush('info',$id);
+		return $id;
+	}
+
+	/**
+	 * Write a warning to the database
+	 * @param $message
+	 * @param $line
+	 * @param $file
+	 * @param $time
+	 *
+	 * @return string
+	 */
+	public function warningWriter($message, $line, $file, $time) {
+		if(!static::$isConnected)
+			$this->connect();
+		$id = hash(config::get('logger_redis_hash_algo'),uniqid('log_w_'));
+		static::$instance->hMSet($id,[
+			'message'   => $message,
+			'line'      => $line,
+			'file'      => $file,
+			'time'      => $time
+		]);
+		static::$instance->lPush('logs'     ,$id);
+		static::$instance->lPush('warning'  ,$id);
+		return $id;
+	}
+
+	/**
+	 * Write a error to the error database
+	 * @param $message
+	 * @param $line
+	 * @param $file
+	 * @param $time
+	 *
+	 * @return string
+	 */
+	public function errorWriter($message, $line, $file, $time) {
+		if(!static::$isConnected)
+			$this->connect();
+		$id = hash(config::get('logger_redis_hash_algo'),uniqid('log_e_'));
+		static::$instance->hMSet($id,[
+			'message'   => $message,
+			'line'      => $line,
+			'file'      => $file,
+			'time'      => $time
+		]);
+		static::$instance->lPush('logs'     ,$id);
+		static::$instance->lPush('errors'   ,$id);
+		return $id;
+	}
+
+	/**
+	 * Return first few items
+	 * @param $limit
+	 *
+	 * @return array
+	 */
+	public function getAll($limit) {
+		if(!static::$isConnected)
+			$this->connect();
+		$limit  = $limit <= 0 ? -1 : $limit;
+		$logs   = static::$instance->lRange('logs',0,$limit);
+		$count  = count($logs);
+		$return = [];
+		for($i  = 0;$i < $count;$i++){
+			$return[]   = static::$instance->hGetAll($logs[$i]);
+		}
+		return $return;
+	}
+}
