3f2de104cc1e877a6d6cf1018e12ea332e5ae81d
Alireza Ghadimi <AlirezaGhadimy@Gmail.com>
2016-09-27 23:05:29 +0330

    Write search with pattern functions

--- server/core/helpers/helper.php
+++ server/core/helpers/helper.php
@@ -30,4 +30,33 @@ class helper {
 	public static function url($url){
 		return str_replace('\\','/',$url);
 	}
+
+	/**
+	 * Convert simple search pattern to regex patterns
+	 *  Symbols:
+	 *      * excepts any character with any length
+	 *      + excepts only one character
+	 * @param $pattern
+	 *
+	 * @return mixed
+	 */
+	public static function pattern2regex($pattern){
+		//* -> except any thing with any length
+		//? -> except only one charter
+		$pattern    = preg_replace('/[^\/]*/','.*',$pattern);
+		return '/^'.preg_replace('/[^\/]?/','.',$pattern).'$/';
+	}
+
+	/**
+	 * Check simple search pattern
+	 * @see   pattern2regex
+	 * @param $pattern
+	 * @param $subject
+	 *
+	 * @return bool
+	 */
+	public static function checkPattern($pattern,$subject){
+		$regex  = self::pattern2regex($pattern);
+		return (bool)preg_match($regex,$subject);
+	}
 }
